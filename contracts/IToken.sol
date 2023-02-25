03fb84348e9f8e58ad4c9be703b180c0bf23c884
Ginika Chinonso <ginika.chinonso.francis@gmail.com>
2023-02-25 22:46:28 +0100

    feat: Script to get the values at storage for some contracts

---
+++ contracts/IToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+interface IToken is IERC20{
+}
